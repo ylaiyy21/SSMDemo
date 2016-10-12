1fd5429d794eac6d43e4474427c7fc418d42a73f
ylaiyy21 <827444827@qq.com>
2016-10-12 11:53:25 +0800

    Revert "redis"
    This reverts commit a3d8523f129310a3f0e24caf17a6f493a8e044bc.

--- src/com/maxplus/redis/RedisDataSource.java
+++
@@ -1,9 +0,0 @@
-package com.maxplus.redis;
-
-import redis.clients.jedis.ShardedJedis;
-
-public interface RedisDataSource {
-	 	public abstract ShardedJedis getRedisClient();
-	    public void returnResource(ShardedJedis shardedJedis);
-	    public void returnResource(ShardedJedis shardedJedis,boolean broken);
-}
